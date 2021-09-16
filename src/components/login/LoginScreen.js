4ced53976e1c08c76a2aedbfd2355b841491a546
Apoyo-Web <mpienso@gmail.com>
2021-09-16 17:28:50 +0200

    logout

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -8,7 +8,7 @@ export const LoginScreen = ({ history }) => {
 
     const handleLogin = () => {
        
-
+        const lastPath = localStorage.getItem('lastPath') || '/'
         dispatch({
             type: types.login,
             payload: {
@@ -16,7 +16,7 @@ export const LoginScreen = ({ history }) => {
             }
         })
 
-        history.replace('/');
+        history.replace(lastPath);
     }
     return (
         <div className="container mt-5">
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    
     
   } from 'react-router-dom'
+import { AuthContext } from '../auth/AuthContext'
 import { LoginScreen } from '../components/login/LoginScreen'
 
 import { DashboardRouter } from './DashboardRouter'
+import { PrivateRouter } from './PrivateRouter'
+import { PublicRouter } from './PublicRouter'
 
 export const AppRouter = () => {
+
+  const {user} = useContext(AuthContext)
     return (
         <Router>
       <div>
         <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
+                    <PublicRouter exact path="/login" component={LoginScreen} isAuthenticated={user.logged} />
                     
-                    <Route path="/" component={DashboardRouter} />
+            <PrivateRouter path="/" component={DashboardRouter} isAuthenticated={user.logged}/>
           </Switch>
       </div>
     </Router>
